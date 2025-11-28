1e8dba39c0be739eb4d3fadfbc770d9440ac507b
pyxyzc <pyxzyc@gmail.com>
2025-11-28 06:31:19 +0000

    [feat] Add cache_out_loc attribute in DumpItem and update build_connector_metadata

--- ucm/integration/sglang/uc_connector.py
+++ ucm/integration/sglang/uc_connector.py
@@ -50,6 +50,7 @@ class DumpItem:
     block_id: str
     start_token_id: int
     end_token_id: int
+    cache_out_loc: torch.Tensor
 
 
 class BlockStatus(Enum):
@@ -576,13 +577,20 @@ class UnifiedCacheConnector():
     def build_connector_metadata(self, schedule_batch: ScheduleBatch) -> UCTransferMetadata:
         meta = UCTransferMetadata()
 
+        running_offset = 0
+
         for i, req in enumerate(schedule_batch.reqs):
             req_id = req.rid
-            req_status = self.req_status_dict[req_id]
+            req_status = self.req_status_dict.get(req_id)
+            if req_status == None:
+                continue
+
             assert req_status.fetch_index % self.block_size == 0, \
             f"fetch_index ({req_status.fetch_index}) must be block-aligned ({self.block_size})"
             assert req_status.dump_index % self.block_size == 0, \
             f"dump_index ({req_status.dump_index}) must be block-aligned ({self.block_size})"
+            assert req_status.dump_end_index % self.block_size == 0, \
+            f"dump_index ({req_status.dump_end_index}) must be block-aligned ({self.block_size})"
 
             prefix_len = schedule_batch.prefix_lens[i]
             extend_len = schedule_batch.extend_lens[i]
@@ -612,17 +620,25 @@ class UnifiedCacheConnector():
             assert extend_begin >= req_status.dump_index
             dump_len = self._convert_len(extend_len)
             dump_begin = extend_begin
-            dump_end = extend_begin + dump_len
+            if req_status.dump_end_index != 0:
+                dump_end = min(req_status.dump_end_index, extend_begin + dump_len)
+            else:
+                dump_end = extend_begin + dump_len
 
             if dump_end > dump_begin:
                 first_dump_block = dump_begin // self.block_size
                 last_dump_block_exclusive = dump_end // self.block_size
                 for blk in range(first_dump_block, last_dump_block_exclusive):
+
+                    cache_start = running_offset + (blk * self.block_size - dump_begin)
+                    cache_end = cache_start + self.block_size
+
                     dump_items.append(
                         DumpItem(
                             block_id=req_status.block_hashes[blk],
                             start_token_id=blk * self.block_size,
                             end_token_id=(blk + 1) * self.block_size,
+                            cache_out_loc=schedule_batch.out_cache_loc[cache_start:cache_end],
                         )
                     )
 
@@ -636,4 +652,6 @@ class UnifiedCacheConnector():
             req_status.prefix_begin_index = prefix_end
             req_status.extend_begin_index = extend_end
 
-        return meta
+            running_offset += extend_len
+
+        self._transfer_metadata = meta
