42462ff768fcf067795e0ff2acf0f66ecfb9979d
xiewanchao <15704912683@163.com>
2025-12-02 14:33:48 +0800

    [fix] Fix the block cache copy logic

--- ucm/integration/sglang/uc_connector.py
+++ ucm/integration/sglang/uc_connector.py
@@ -272,10 +272,12 @@ class UnifiedCacheConnector():
                 first_half = cuda_blocks[:mid]      # K blocks
                 second_half = cuda_blocks[mid:]     # V blocks
 
-                # assert len(first_half) == len(second_half) == num_blocks
+                assert len(first_half) == len(second_half) == num_blocks
                 for k_cb, v_cb, blk_id in zip(first_half, second_half, block_ids):
-                    kv_layer[0][blk_id] = self.cuda_block_pool.get_block(k_cb)
-                    kv_layer[1][blk_id] = self.cuda_block_pool.get_block(v_cb)
+                    k_block = self.cuda_block_pool.get_block(k_cb)
+                    kv_layer[0][blk_id] = k_block
+                    v_block = self.cuda_block_pool.get_block(v_cb)
+                    kv_layer[1][blk_id] = v_block
             
             self._free_task_cuda_blocks(task)
 
